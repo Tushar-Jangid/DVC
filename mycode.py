6d96c651e2d05b46d75ef5468709373ca9e5ed05
Tushar-Jangid <tusharjangid064@gmail.com>
2026-02-04 11:48:54 +0530

    2nd version

--- mycode.py
+++ mycode.py
@@ -9,6 +9,12 @@ data = {
 
 df = pd.DataFrame(data)
 
+new_row = {'Name': 'Ankit', 'Age': 22, 'City': 'Mumbai'}
+df.loc[len(df.index)] = new_row
+
+new_row = {'Name': 'Anshul', 'Age': 22, 'City': 'Mussoorie'}
+df.loc[len(df.index)] = new_row
+
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 
